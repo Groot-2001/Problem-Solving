8721a7e06061d5aa5b9451eb6443bef407e62093
Groot-2001 <shivasilmawala841@gmail.com>
2023-12-14 23:24:10 +0530

    another solution added to finding duplicates in an array

--- Array/Easy/duplicate contains.java
+++ Array/Easy/duplicate contains.java
@@ -13,3 +13,24 @@ public class Solution {
         return false;
     }
 }
+
+/**
+ * class Solution {
+ * public boolean containsDuplicate(int[] nums) {
+ * 
+ * Arrays.sort(nums); //n log(n)
+ * 
+ * for(int i=0; i<nums.length;i++){
+ * if(nums[i]==nums[i+1]){ //o(n)
+ * return true;
+ * }
+ * }
+ * 
+ * return false;
+ * 
+ * }
+ * }
+ * 
+ * Time Complexity : O(nlogn) + O(n)
+ * Space Complexity : O(n)
+ */
